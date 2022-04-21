f30786f71a5ac9bf7d286a2d6d0e9a81e0159121
Dmitry Korovin <korovin.ds@aurrency.com>
2022-04-22 00:51:04 +0300

    Add reward of two stakers comparison

--- tests/test_stake.py
+++ tests/test_stake.py
@@ -42,5 +42,35 @@ def test_stake(accounts, staking_contracts):
 
 	assert reward2 < reward1
 	assert reward2 < reward3
-	assert reward3 - reward1 < 1e14
-	assert reward3 // reward2 == 2 
+	assert reward3 - reward1 < 1e14 # 1e14 - eps
+	assert reward3 // reward2 == 2
+
+def test_getReward_after_withdraw(accounts, staking_contracts):
+	(staked_token, reward_token, staking) = staking_contracts
+
+	time1 = chain.time()
+	staking.stake(1e17, {'from': accounts[1]})
+
+	chain.sleep(1000)
+
+	tx1 = staking.getReward({"from": accounts[1]})
+	reward1 = tx1.events["RewardPaid"]["reward"]
+	staking.stake(1e17, {"from": accounts[2]})
+
+	time2 = chain.time()
+	chain.sleep(1000)
+
+	tx2 = staking.getReward({"from": accounts[1]})
+	reward2 = tx2.events["RewardPaid"]["reward"]
+	staking.withdraw(1e17, {"from": accounts[2]})
+
+	time3 = chain.time()
+	chain.sleep(1000)
+
+	tx3 = staking.getReward({"from": accounts[1]})
+	reward3 = tx3.events["RewardPaid"]["reward"]
+	
+	tx4 = staking.getReward({"from": accounts[2]})
+	reward4 = tx4.events["RewardPaid"]["reward"]
+
+	assert reward4 - reward2 < 1e14
